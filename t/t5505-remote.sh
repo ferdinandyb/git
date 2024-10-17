d1098fedddfd6243d03ea45b6243634acd3d2e78
Bence Ferdinandy <bence@ferdinandy.com>
2024-10-17 22:11:45 +0200

    testing

--- t/t5505-remote.sh
+++ t/t5505-remote.sh
@@ -724,8 +724,11 @@ test_expect_success 'update' '
 		cd one &&
 		git remote add drosophila ../two &&
 		git remote add apis ../mirror &&
+		git ls-remote drosophila &&
+		git ls-remote apis &&
 		git remote update &&
 		git branch -r >output &&
+		echo "force error" >output &&
 		test_cmp expect output
 	)
 '
